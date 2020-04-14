7c454d465d8e42723cc9be107af8aa8f2b7e77c8
Adoendithas <adrian.algasinger@web.de>
2020-04-14 13:30:33 +0200

    master_ini

--- src/js/PetFight.js
+++ src/js/PetFight.js
@@ -1,6 +1,6 @@
 import { GameTemplate } from "../../vendor/gamebox/src/js/games/GameTemplate.js";
-import { GameTemplate } from "../../vendor/gamebox/src/js/games/GameTemplate.js";
-import { GameTemplate } from "../../vendor/gamebox/src/js/games/GameTemplate.js";
+import { Battle } from "../../src/js/Battle.js";
+import { Map } from "../../src/js/Map.js";
 // Import je nachdem was wir brauchen: => import { GameObject, MovableGameObject, Ball, Mode } from "../GameObject.js";
 
 export class PetFight extends GameTemplate{
