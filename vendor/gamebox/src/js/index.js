0a74e6eab8f0938755ac7dbd1b7c454fb950e12f
Louis Ritzkowski <louis@pixelwizart.de>
2020-09-20 13:39:15 +0200

    Fix Skinchanger Skin

--- vendor/gamebox/src/js/index.js
+++ vendor/gamebox/src/js/index.js
@@ -12,7 +12,7 @@ let skinStyle = document.querySelector("#skin"),
     // save files in array for css, same for colors, attributes, ....
     // use these in html
 
-window.skinChanger = new SkinChanger(skinStyle, skins, "../vendor/gamebox/src/css/");
+window.skinChanger = new SkinChanger(skinStyle, skins, "./vendor/gamebox/src/css/");
 
 document.querySelector(".next").addEventListener("click", () => skinChanger.next());
 document.querySelector(".previous").addEventListener("click", () => skinChanger.previous());
